b37d4a7de6bb3b6a8e36c774f59c0cc3f273fd26
Jonathan <contrerasivan1609@gmail.com>
2025-02-23 02:25:08 -0600

    POO

---
+++ JICC/Usuario/MainRU.java
@@ -0,0 +1,61 @@
+
+// Libreria Scanner
+import java.util.Scanner;
+
+// Clase MainRU {
+class MainRU {
+    // Método main
+    public static void main(String[] args) {
+        // Creación de un objeto Scanner
+        Scanner sc = new Scanner(System.in);
+        // Creación de un objeto RestaU
+        RestaU us1 = new RestaU();
+        // Declaración de la variable opc
+        int opc;
+        // Bucle do-while
+        do {
+            // Muestra las opciones en pantalla
+            System.out.println("-----------Elija una opcion----------");
+            System.out.println("1.- Hacer la resta");
+            System.out.println("2.- Salir");
+            // Pide la opción al usuario
+            System.out.println("Ingrese su opcion: ");
+            // Guarda la opción del usuario
+            opc = sc.nextInt();
+            // Estructura Switch, verifica la opción del usuario
+            switch (opc) {
+                // Hacer la resta
+                case 1:
+                    // Pide el primer numero al usuario
+                    System.out.println("Ingrese el primer numero: ");
+                    // Asigna el primer numero del usuario a la variable num1
+                    us1.setNum1(sc.nextInt());
+                    // Pide el segundo numero al usuario
+                    System.out.println("Ingrese el segundo numero: ");
+                    // Asigna el segundo numero del usuario a la variable num2
+                    us1.setNum2(sc.nextInt());
+                    // Calcula el resultado de la resta y se guarda en la variable r
+                    int r = us1.getNum1() - us1.getNum2();
+                    // Imprime el resultado
+                    System.out.println("El resultado es: " + r);
+                    // Se rompe el flujo del switch después de ejecutar el caso 1 y regresa al menu
+                    break;
+                // Sale del programa
+                case 2:
+                    // Imprime el mensaje de salida
+                    System.out.println("Saliendo del programa");
+                    // Se rompe el flujo del switch después de ejecutar el caso 2
+                    break;
+                // Caso por defecto
+                default:
+                    // Imprime el mensaje de error
+                    System.out.println("Invalida intente de nuevo");
+                    // Se rompe el flujo del switch después de ejecutar el caso por defecto y
+                    // regresa al menu
+                    break;
+            }
+            // Se rompe el bucle do-while después de ejecutar el caso 2
+        } while (opc != 2);
+        sc.close();
+    }
+}
---
+++ JICC/Usuario/SumaU.java
@@ -0,0 +1,33 @@
+// Definición de la clase SumaU
+class SumaU {
+    // Declaración de los atributos de la clase
+    int num1, num2;
+
+    // Constructor vacío
+    SumaU() {
+    }
+
+    // Método getNum1
+    public int getNum1() {
+        // Devuelve el valor de num1
+        return num1;
+    }
+
+    // Método setNum1
+    public void setNum1(int num1) {
+        // Asigna el valor de num1 a la variable
+        this.num1 = num1;
+    }
+
+    // Método getNum2
+    public int getNum2() {
+        // Devuelve el valor de num2
+        return num2;
+    }
+
+    // Método setNum2
+    public void setNum2(int num2) {
+        // Asigna el valor de num2 a la variable
+        this.num2 = num2;
+    }
+}
